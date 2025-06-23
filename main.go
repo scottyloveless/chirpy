3328186b185e182aaa848b42ca4201e8006333ff
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2025-06-23 07:26:23 -0500

    app folder, readiness handler

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	const filepathRoot = "."
+	const filepathRoot = "./app"
 	const port = "8080"
+	fs := http.FileServer(http.Dir("./app"))
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/", http.StripPrefix("/app/", fs))
+	mux.HandleFunc("/healthz/", readinessHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -19,4 +22,19 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	const body = "OK"
+	const contentTypeKey = "Content-Type"
+	const contentTypeValue = "text/plain; charset=utf-8"
+	const statusCode = 200
+	const resBody = "OK"
+
+	w.Header().Add(contentTypeKey, contentTypeValue)
+	w.WriteHeader(statusCode)
+	_, err := w.Write([]byte(resBody))
+	if err != nil {
+		fmt.Printf("error writing response: %v", err)
+	}
 }
